00d554f4621622bdfa72a0a29e145b5b553786ba
Vincent Tollu <vincent.tollu@gmail.com>
2012-09-05 22:48:29 +0200

    first code commit

---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Page principale";
+            NewListViewModel model = new NewListViewModel();
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/WebApp/Models/NewListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class NewListViewModel
+    {
+        public string SelectedItemId { get; set; }
+        public List<string> brands { get; set; }
+
+        public NewListViewModel()
+        {
+            brands = new List<string>();
+            brands.Add("kiabi");
+            brands.Add("toto");
+        }
+    }
+}
